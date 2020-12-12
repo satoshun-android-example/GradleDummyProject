6d2de12fb59d563011f30d28e98f03dc45aa31dc
SatoShun <shun.sato1@gmail.com>
2020-12-12 03:35:04 +0000

    Internal

---
+++ lib/src/main/java/com/github/satoshun/example/internal/InternalClass653.kt
@@ -0,0 +1,31 @@
+package com.github.satoshun.example.internal
+
+import kotlinx.coroutines.delay
+
+private suspend fun test() {
+  delay(100)
+}
+
+private suspend fun tes2() {
+  delay(100)
+}
+
+private suspend fun tes3() {
+  delay(100)
+}
+
+private suspend fun tes4() {
+  delay(100)
+}
+
+private suspend fun tes5() {
+  delay(100)
+}
+
+private suspend fun tes6() {
+  delay(100)
+}
+
+private suspend fun tes7() {
+  delay(100)
+}
