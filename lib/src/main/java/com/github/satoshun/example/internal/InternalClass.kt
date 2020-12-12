8f0d7524922f8e11ff5185a9822f256d5a26e404
SatoShun <shun.sato1@gmail.com>
2020-12-12 03:08:24 +0000

    add dummy class

---
+++ lib/src/main/java/com/github/satoshun/example/internal/InternalClass.kt
@@ -0,0 +1,123 @@
+package com.github.satoshun.example.internal
+
+import kotlinx.coroutines.delay
+
+private class InternalClass2 {
+  suspend fun test() {
+    delay(100)
+  }
+
+  suspend fun tes2() {
+    delay(100)
+  }
+
+  suspend fun tes3() {
+    delay(100)
+  }
+
+  suspend fun tes4() {
+    delay(100)
+  }
+
+  suspend fun tes5() {
+    delay(100)
+  }
+
+  suspend fun tes6() {
+    delay(100)
+  }
+
+  suspend fun tes7() {
+    delay(100)
+  }
+}
+
+private class InternalClass3 {
+  suspend fun test() {
+    delay(100)
+  }
+
+  suspend fun tes2() {
+    delay(100)
+  }
+
+  suspend fun tes3() {
+    delay(100)
+  }
+
+  suspend fun tes4() {
+    delay(100)
+  }
+
+  suspend fun tes5() {
+    delay(100)
+  }
+
+  suspend fun tes6() {
+    delay(100)
+  }
+
+  suspend fun tes7() {
+    delay(100)
+  }
+}
+
+private class InternalClas4 {
+  suspend fun test() {
+    delay(100)
+  }
+
+  suspend fun tes2() {
+    delay(100)
+  }
+
+  suspend fun tes3() {
+    delay(100)
+  }
+
+  suspend fun tes4() {
+    delay(100)
+  }
+
+  suspend fun tes5() {
+    delay(100)
+  }
+
+  suspend fun tes6() {
+    delay(100)
+  }
+
+  suspend fun tes7() {
+    delay(100)
+  }
+}
+
+private class InternalClass5 {
+  suspend fun test() {
+    delay(100)
+  }
+
+  suspend fun tes2() {
+    delay(100)
+  }
+
+  suspend fun tes3() {
+    delay(100)
+  }
+
+  suspend fun tes4() {
+    delay(100)
+  }
+
+  suspend fun tes5() {
+    delay(100)
+  }
+
+  suspend fun tes6() {
+    delay(100)
+  }
+
+  suspend fun tes7() {
+    delay(100)
+  }
+}
